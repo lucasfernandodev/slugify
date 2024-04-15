509f58deb0d57fc9a40b91d4ba1530e6e89516ba
Lucas Fernando <lucasfernando.dev@gmail.com>
2024-04-15 18:15:10 -0300

    ♻️ refactr:Transform button to link

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,6 +1,7 @@
 import { IconChevronsRight } from '@tabler/icons-react';
 import { RootLayout } from '../../components/Layout/root';
 import style from './style.module.css'
+import { Link } from '../../infra/Link';
 
 export const About = () => {
   return (
@@ -8,7 +9,7 @@ export const About = () => {
       <div className={style.container}>
         <h1>Sobre o gerador de slugify</h1>
         <p>O gerador de slugify é uma ferramenta simplista, porém poderosa, que é de grande ajuda na hora de criar slugs amigáveis para suas postagens. Se quiser saber como funciona, dê uma olhada em nosso código no GitHub.</p>
-        <button><span>Conhecer no Github</span> <IconChevronsRight /></button>
+        <Link target='_blank' to="https://github.com/lucasfernandodev/slugify"><span>Conhecer no Github</span> <IconChevronsRight /></Link>
       </div>
     </RootLayout>
   );
--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,6 +1,7 @@
 import { IconChevronsRight } from '@tabler/icons-react';
 import { RootLayout } from '../../components/Layout/root';
 import style from './style.module.css'
+import { Link } from '../../infra/Link';
 
 export const Contact = () => {
   return (
@@ -8,7 +9,7 @@ export const Contact = () => {
       <div className={style.container}>
         <h1>Contato</h1>
         <p>Se tiver alguma dúvida, entre em contato conosco no repositório oficial do projeto no GitHub.</p>
-        <button><span>Conhecer no Github</span> <IconChevronsRight /></button>
+        <Link target='_blank' to="https://github.com/lucasfernandodev/slugify"><span>Conhecer no Github</span> <IconChevronsRight /></Link>
       </div>
     </RootLayout>
   );
